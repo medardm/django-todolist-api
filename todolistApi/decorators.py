cb4792cbccb5436900d9bccf55ede4b20a1d6b99
Medard Mandane <medardm13@gmail.com>
2024-05-08 03:17:02 +0800

    Add JWT authentication decorator
    - Created a new file 'decorators.py' in 'todolistApi' with JWT based authentication decorator.
    - The 'jwt_required' decorator checks if 'HTTP_COOKIE' is present in user request.
    - It tries to decode the JWT using the secret key. If successful, it allows the user to proceed.
    - The decorator returns appropriate error messages and HTTP status codes for cases where JWT is invalid or user is not authenticated.

---
+++ todolistApi/decorators.py
@@ -0,0 +1,31 @@
+import jwt
+from jwt.exceptions import InvalidTokenError
+from django.http import JsonResponse
+from todolistApi.settings import SECRET_JWT
+from functools import wraps
+
+
+def jwt_required(f=None, *, disallow_authenticated=False):
+    def decorator(func):
+        @wraps(func)
+        def wrap(self, request, *args, **kwargs):
+            if 'HTTP_COOKIE' in request.META:
+                token = request.COOKIES.get('token')
+                if token is not None:
+                    try:
+                        jwt.decode(token, SECRET_JWT, algorithms='HS256')
+                        if disallow_authenticated:
+                            return JsonResponse({'error': 'You are not allowed to perform this action.'}, status=400)
+                        return func(self, request, *args, **kwargs)
+                    except InvalidTokenError:
+                        if not disallow_authenticated:
+                            return JsonResponse({'error': 'Invalid token. Please log in again.'}, status=401)
+            if not disallow_authenticated:
+                return JsonResponse({'error': 'You must be logged in to perform this action.'}, status=401)
+            return func(self, request, *args, **kwargs)
+
+        return wrap
+
+    if f:
+        return decorator(f)
+    return decorator
