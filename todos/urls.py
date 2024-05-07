378b6a3ae3e92ffbbf4a943fc32ab34f5b832c6b
Medard Mandane <medardm13@gmail.com>
2024-05-08 04:32:12 +0800

    Refactor views in todo app with class-based views
    - Replaced function-based views with class-based views(APIView) in `todos/views.py`
    - See `TodoListView` and `TodoListDetailView` for changes
    - Updated get and post methods for getting and creating todos
    - Updated get, put and delete methods for fetching, updating and deleting a specific todo
    - Changed HttpResponse in methods to Status with more user-friendly messages
    - Updated URL patterns in `todos/urls.py` to call the new class-based views

--- todos/urls.py
+++ todos/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views as todos_views
 
 urlpatterns = [
-    path('', todos_views.todo_lists, name='get_todo_lists'),
-    path('', todos_views.todo_lists, name='create_todo_list'),
-    path('<int:pk>/', todos_views.todo_list_detail, name='todo_list_detail'),
+    path('', todos_views.TodoListView.as_view()),
+    path('<int:pk>/', todos_views.TodoListDetailView.as_view(), name='todo_list_detail'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -4,42 +4,57 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import TodoList, TodoItem
 from .serializers import TodoListSerializer, TodoItemsSerializer
 
 
-@api_view(['GET', 'POST'])
-def todo_lists(request, format=None):
-    if request.method == 'GET':
+class TodoListView(APIView):
+    """
+    List all todo items, or create a new todo item.
+    """
+
+    def get(self, request, format=None):
         todolists = TodoList.objects.all()
         serializer = TodoListSerializer(todolists, many=True)
-        return Response({'success': 'true', 'data': serializer.data}, status=status.HTTP_200_OK)
+        return Response({'success': 'true', 'message': 'Todo lists fetched successfully.', 'data': serializer.data},
+                        status=status.HTTP_200_OK)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = TodoListSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'success': 'true', 'data': serializer.data}, status=status.HTTP_201_CREATED)
-        return Response({'success': 'false', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'success': 'true', 'message': 'Todo list created successfully.', 'data': serializer.data},
+                            status=status.HTTP_201_CREATED)
+        return Response({'success': 'false', 'message': 'Error creating todo list.', 'errors': serializer.errors},
+                        status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def todo_list_detail(request, pk, format=None):
-    todo_list = get_object_or_404(TodoList, pk=pk)
+class TodoListDetailView(APIView):
+    """
+    Retrieve, update or delete a todo item.
+    """
 
-    if request.method == 'GET':
+    def get_object(self, pk):
+        return get_object_or_404(TodoList, pk=pk)
+
+    def get(self, request, pk, format=None):
+        todo_list = self.get_object(pk)
         serializer = TodoListSerializer(todo_list)
-        return JsonResponse({'success': 'true', 'data': serializer.data}, safe=False)
+        return Response({'success': 'true', 'message': 'Todo list fetched successfully.', 'data': serializer.data})
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        todo_list = self.get_object(pk)
         serializer = TodoListSerializer(todo_list, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse({'success': 'true', 'data': serializer.data}, safe=False)
-        return JsonResponse({'success': 'false', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'success': 'true', 'message': 'Todo list updated successfully.', 'data': serializer.data})
+        return Response({'success': 'false', 'message': 'Error updating todo list.', 'errors': serializer.errors},
+                        status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        todo_list = self.get_object(pk)
         todo_list.delete()
-        return JsonResponse({'success': 'true', 'message': 'Todo list was deleted successfully!'},
-                            status=status.HTTP_204_NO_CONTENT)
+        return Response({'success': 'true', 'message': 'Todo list was deleted successfully!'},
+                        status=status.HTTP_204_NO_CONTENT)
