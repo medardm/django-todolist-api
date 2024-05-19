5b59affffd6922a008fddebcc86c3c627ebc2909
Medard Mandane <medardm13@gmail.com>
2024-05-19 23:52:35 +0800

    Update allowed hosts in settings
    The settings file has been updated to include an AWS EC2 instance as an allowed host.
    
    - Updated `ALLOWED_HOSTS` list in settings.py.
    - Added 'ec2-54-162-144-198.compute-1.amazonaws.com' to the list of allowed hosts. This allows the Django application to accept requests from this address.

--- todolistApi/settings.py
+++ todolistApi/settings.py
@@ -30,7 +30,9 @@ SECRET_JWT = os.getenv('SECRET_JWT', 'sample_jwt_secret_key_12345678')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-54-162-144-198.compute-1.amazonaws.com'
+]
 
 # Application definition
 
